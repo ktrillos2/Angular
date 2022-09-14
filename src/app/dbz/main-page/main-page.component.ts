3b194bc36538703d0ce5f8d9200b511bec9ed9b9
Kumo <ktrillos2@gmail.com>
2022-09-13 19:21:01 -0500

    Curso Angular

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,9 +1,5 @@
 import { Component } from '@angular/core';
-
-interface Personaje {
-  nombre: string;
-  poder: number;
-}
+import { Personaje } from '../interfaces/dbz.interface';
 
 @Component({
   selector: 'app-dbz',
@@ -18,7 +14,7 @@ export class MainPageComponent {
     {
       nombre: 'Vegetta',
       poder: 7500,
-    }
+    },
   ];
 
   nuevo: Personaje = {
@@ -30,10 +26,10 @@ export class MainPageComponent {
     if (this.nuevo.nombre.trim().length == 0) {
       return;
     }
-    this.personajes.push(this.nuevo)
-    this.nuevo={
+    this.personajes.push(this.nuevo);
+    this.nuevo = {
       nombre: '',
       poder: 0,
-    }
+    };
   }
 }
